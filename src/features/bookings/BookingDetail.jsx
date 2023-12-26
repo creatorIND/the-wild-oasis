483d00b32616d90bb0a140ab0e91ae139af42560
Sujal Sinha <sujalsinha2243@gmail.com>
2023-12-26 22:31:31 +0530

    add functionality to delete a booking

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -10,10 +10,13 @@ import Button from "../../ui/Button";
 import ButtonText from "../../ui/ButtonText";
 import Spinner from "../../ui/Spinner";
 import BookingDataBox from "./BookingDataBox";
+import Modal from "../../ui/Modal";
+import ConfirmDelete from "../../ui/ConfirmDelete";
 
 import { useMoveBack } from "../../hooks/useMoveBack";
 import { useBooking } from "./useBooking";
 import { useCheckout } from "../check-in-out/useCheckout";
+import { useDeleteBooking } from "./useDeleteBooking";
 
 const HeadingGroup = styled.div`
 	display: flex;
@@ -24,6 +27,7 @@ const HeadingGroup = styled.div`
 function BookingDetail() {
 	const { booking, isLoading } = useBooking();
 	const { checkout, isCheckingOut } = useCheckout();
+	const { deleteBooking, isDeleting } = useDeleteBooking();
 	const moveBack = useMoveBack();
 	const navigate = useNavigate();
 
@@ -66,6 +70,22 @@ function BookingDetail() {
 						Check out
 					</Button>
 				)}
+				<Modal>
+					<Modal.Open opens="delete">
+						<Button variation="danger">Delete booking</Button>
+					</Modal.Open>
+					<Modal.Window name="delete">
+						<ConfirmDelete
+							resourceName="booking"
+							disabled={isDeleting}
+							onConfirm={() =>
+								deleteBooking(bookingId, {
+									onSettled: () => navigate(-1),
+								})
+							}
+						/>
+					</Modal.Window>
+				</Modal>
 				<Button variation="secondary" onClick={moveBack}>
 					Back
 				</Button>
