72c5769565c463dee17ba429fc0bc3d30caf65b0
Sujal Sinha <sujalsinha2243@gmail.com>
2025-02-28 20:40:00 +0530

    minor changes to sample data uploader and adding it back to Sidebar

--- src/data/Uploader.jsx
+++ src/data/Uploader.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 import { isFuture, isPast, isToday } from "date-fns";
 import supabase from "../services/supabase";
 
@@ -7,38 +9,10 @@ import Button from "../ui/Button";
 import { subtractDates } from "../utils/helpers";
 import { bookings } from "./data-bookings";
 import { cabins } from "./data-cabins";
-import { guests } from "./data-guests";
-
-// const originalSettings = {
-//   minBookingLength: 3,
-//   maxBookingLength: 30,
-//   maxGuestsPerBooking: 10,
-//   breakfastPrice: 15,
-// };
-
-async function deleteGuests() {
-	const { error } = await supabase.from("guests").delete().gt("id", 0);
-	if (error) console.log(error.message);
-}
-
-async function deleteCabins() {
-	const { error } = await supabase.from("cabins").delete().gt("id", 0);
-	if (error) console.log(error.message);
-}
 
 async function deleteBookings() {
 	const { error } = await supabase.from("bookings").delete().gt("id", 0);
-	if (error) console.log(error.message);
-}
-
-async function createGuests() {
-	const { error } = await supabase.from("guests").insert(guests);
-	if (error) console.log(error.message);
-}
-
-async function createCabins() {
-	const { error } = await supabase.from("cabins").insert(cabins);
-	if (error) console.log(error.message);
+	if (error) console.error(error.message);
 }
 
 async function createBookings() {
@@ -95,56 +69,42 @@ async function createBookings() {
 		};
 	});
 
-	console.log(finalBookings);
-
 	const { error } = await supabase.from("bookings").insert(finalBookings);
-	if (error) console.log(error.message);
+	if (error) console.error(error.message);
 }
 
 function Uploader() {
 	const [isLoading, setIsLoading] = useState(false);
-
-	async function uploadAll() {
-		setIsLoading(true);
-		// Bookings need to be deleted FIRST
-		await deleteBookings();
-		await deleteGuests();
-		await deleteCabins();
-
-		// Bookings need to be created LAST
-		await createGuests();
-		await createCabins();
-		await createBookings();
-
-		setIsLoading(false);
-	}
+	const queryClient = useQueryClient();
 
 	async function uploadBookings() {
 		setIsLoading(true);
-		await deleteBookings();
-		await createBookings();
-		setIsLoading(false);
+		try {
+			await deleteBookings();
+			await createBookings();
+			queryClient.invalidateQueries({ queryKey: ["bookings"] });
+		} catch (error) {
+			console.error(error.message);
+			toast.error("There was some error uploading sample data");
+		} finally {
+			setIsLoading(false);
+			toast.success("Sample data uploaded");
+		}
 	}
 
 	return (
 		<div
 			style={{
 				marginTop: "auto",
-				backgroundColor: "#e0e7ff",
+				backgroundColor: "var(--color-grey-100)",
 				padding: "8px",
 				borderRadius: "5px",
 				textAlign: "center",
 				display: "flex",
 				flexDirection: "column",
 				gap: "8px",
-			}}
-		>
-			<h3>SAMPLE DATA</h3>
-
-			<Button onClick={uploadAll} disabled={isLoading}>
-				Upload ALL
-			</Button>
-
+			}}>
+			<h4>SAMPLE DATA</h4>
 			<Button onClick={uploadBookings} disabled={isLoading}>
 				Upload bookings ONLY
 			</Button>
